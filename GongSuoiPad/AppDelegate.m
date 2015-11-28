342d15e0bb044bcdfc66037dcb180454f851a19c
7405321@gmail.com <7405321@gmail.com>
2015-11-28 10:52:34 +0800

    Add gongsuo project
    Add gongsuo project

--- GongSuoiPad/AppDelegate.m
+++ GongSuoiPad/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "RootViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,12 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    self.window.backgroundColor = [UIColor whiteColor];
+    RootViewController *rootControll = [[RootViewController alloc] init];
+    self.window.rootViewController = rootControll;
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
